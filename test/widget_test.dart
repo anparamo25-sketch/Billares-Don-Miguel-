366c18670a2b9fe10ca2b80a0be5614d20a83c9e
anparamo25-sketch <anparamo25@gmail.com>
2026-09-08 08:56:56 -0600

    Actualizar prueba basica de inicio

--- test/widget_test.dart
+++ test/widget_test.dart
@@ -2,8 +2,9 @@ import 'package:flutter_test/flutter_test.dart';
 import 'package:billares_don_miguel/main.dart';
 
 void main() {
-  testWidgets('La aplicación inicia correctamente', (tester) async {
+  testWidgets('La aplicación inicia en la pantalla de administrador', (tester) async {
     await tester.pumpWidget(const BillaresApp());
+    await tester.pump(const Duration(milliseconds: 100));
     expect(find.byType(LoginPage), findsOneWidget);
   });
 }
