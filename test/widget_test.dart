47b15c021c2002615517d35c1cf608e8723e4177
anparamo25-sketch <anparamo25@gmail.com>
2026-09-08 08:48:55 -0600

    Corregir prueba de arranque

--- test/widget_test.dart
+++ test/widget_test.dart
@@ -2,8 +2,8 @@ import 'package:flutter_test/flutter_test.dart';
 import 'package:billares_don_miguel/main.dart';
 
 void main() {
-  testWidgets('La pantalla de acceso muestra Billares Don Miguel', (tester) async {
+  testWidgets('La aplicación inicia correctamente', (tester) async {
     await tester.pumpWidget(const BillaresApp());
-    expect(find.text('Billares Don Miguel'), findsOneWidget);
+    expect(find.byType(LoginPage), findsOneWidget);
   });
 }
