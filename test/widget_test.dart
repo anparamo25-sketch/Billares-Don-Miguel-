4d1e921ba0cfd822e5508ce29f195a68064c9f18
anparamo25-sketch <anparamo25@gmail.com>
2026-09-09 23:05:44 -0600

    fix: update widget test for 1.2.7

--- test/widget_test.dart
+++ test/widget_test.dart
@@ -8,13 +8,13 @@ void main() {
     expect(find.byType(LoginPage), findsOneWidget);
   });
 
-  test('Las cinco tarifas de 1.2.6 permanecen bloqueadas con los valores acordados', () {
+  test('Las cinco tarifas permanecen bloqueadas con los valores acordados', () {
     expect(tableRates[1], 120);
     expect(tableRates[2], 120);
     expect(tableRates[3], 100);
     expect(tableRates[4], 100);
     expect(tableRates[5], 70);
-    expect(appVersion, '1.2.6+126');
+    expect(appVersion, '1.2.7+127');
   });
 
   test('Los estados funcionales de una mesa existen sin arquitectura de dispositivos por mesa', () {
