a040bc4bd5d2873abb10af8bbaa3130080eacf25
anparamo25-sketch <anparamo25@gmail.com>
2026-09-12 23:07:56 -0600

    test: update expected version for v1.3.0

--- test/widget_test.dart
+++ test/widget_test.dart
@@ -16,7 +16,7 @@ void main() {
     expect(tableRates[3], 100);
     expect(tableRates[4], 100);
     expect(tableRates[5], 70);
-    expect(appVersion, '1.2.9+129');
+    expect(appVersion, '1.3.0+130');
   });
 
   test('Los estados funcionales de una mesa existen sin arquitectura de dispositivos por mesa', () {
